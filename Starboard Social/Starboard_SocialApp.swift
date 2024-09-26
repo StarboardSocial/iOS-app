682bf8093773a2b9aa68ff5ca2f267d36c6540e6
Josian van Efferen <github@Josian.nl>
2024-09-26 10:57:35 +0200

    Initial Commit

---
+++ Starboard Social/Starboard_SocialApp.swift
@@ -0,0 +1,17 @@
+//
+//  Starboard_SocialApp.swift
+//  Starboard Social
+//
+//  Created by Josian van Efferen on 26/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct Starboard_SocialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
